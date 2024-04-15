7cc6be5bc5ef65cf5d64d5b0d645a0f262e0e397
Fankrbb <fankrbb@gmail.com>
2024-04-15 14:20:00 +0700

    feat(auth): add firebase auth initialize

--- src/main.js
+++ src/main.js
@@ -3,4 +3,18 @@ import App from './App.vue'
 import router from './router'
 import './style.css'
 
-createApp(App).use(router).mount('#app')
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBerNcub5qykfOHKBxYoz9Vl1cqG1mm1g",
+  authDomain: "egco427-1d490.firebaseapp.com",
+  projectId: "egco427-1d490",
+  storageBucket: "egco427-1d490.appspot.com",
+  messagingSenderId: "897620003440",
+  appId: "1:897620003440:web:40e94d1b6684c0192042b5",
+  measurementId: "G-6C5P86XSNY"
+};
+
+initializeApp(firebaseConfig);
+
+const app = createApp(App).use(router).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import CityList from '../views/CityList.vue'
 import Info from '../views/Info.vue'
 import SignIn from '../views/SignIn.vue'
@@ -7,32 +8,59 @@ import SignUp from '../views/SignUp.vue'
 const routerHistory = createWebHistory()
 
 const routes = [
-    {
-        path: '/cities',
-        name: 'CityList',
-        component: CityList
-    },
-    {
-        path: '/info/:city',
-        name: 'Info',
-        component: Info
-    },
-    {
-        path: '/signin',
-        name: 'SignIn',
-        component: SignIn
-    },
-    {
-        path: '/signup',
-        name: 'Signp',
-        component: SignUp
-    }
+  {
+    path: '/cities',
+    name: 'CityList',
+    component: CityList
+  },
+  {
+    path: '/info/:city',
+    name: 'Info',
+    component: Info
+  },
+  {
+    path: '/signin',
+    name: 'SignIn',
+    component: SignIn
+  },
+  {
+    path: '/signup',
+    name: 'Signp',
+    component: SignUp
+  }
 ]
 
 const router = createRouter({
-	history: createWebHistory(),
-	routes
+  history: createWebHistory(),
+  routes
 })
 
+const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const removeListener = onAuthStateChanged(
+      getAuth(),
+      (user) => {
+        removeListener()
+        resolve(user)
+      },
+      reject
+    )
+  })
+}
+
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth)
+  if (requiresAuth) {
+    if (await getCurrentUser()) {
+      console.log("You are authorized to access this area.");
+      next()
+    } else {
+      console.log("You are not authorized to access this area.")
+      next('signin')
+    }
+  } else {
+    next()
+  }
+})
 
 export default router
