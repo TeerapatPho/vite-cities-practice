6d7530bcc96ae2f0eae326741e7e0bbe0c271f13
Fankrbb <fankrbb@gmail.com>
2024-04-15 14:55:04 +0700

    feat(db): retrieve data from firestore, check for auth

--- src/views/CityList.vue
+++ src/views/CityList.vue
@@ -3,55 +3,76 @@
     <h4>City List</h4>
     <br>
     <div class="row">
-    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"/>
-    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-    <div class="panel panel-info" >
-      <div class="panel-heading">
-        <h3 class="panel-title">List of Cities</h3>
-      </div>
-      <div class="panel-body">
-        <div class="row">
-      <div class="col-sm-6 col-md-4" >
-        <div class="thumbnail">
-          <img src="" width=80%>
-          <div class="caption">
-            <p><b>City Name</b></p>
-            <p class="citydetail">City Detail</p>
-            <button class="btn btn-success" role="button">Detail</button>
+      <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" />
+      <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+        <div class="panel panel-info">
+          <div class="panel-heading">
+            <h3 class="panel-title">List of Cities</h3>
+          </div>
+          <div class="panel-body">
+            <div class="row">
+              <div class="col-sm-6 col-md-4" v-for="(city, key) in cities" :key='key'>
+                <div class="thumbnail">
+                  <img :src="`../assets/${city.image}`" width=80%>
+                  <div class="caption">
+                    <p><b>{{ city.name }}</b></p>
+                    <p class="citydetail">{{ city.detail }}</p>
+                    <button class="btn btn-success" role="button">Detail</button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-      </div>
-    </div>
-    </div>
-    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"/>
+      <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" />
     </div>
     <div class="row">
-    <div class="container">
-      <button class="btn btn-default"><span class="glyphicon glyphicon-log-out"></span> Logout</button>
-    </div>
+      <div class="container">
+        <button class="btn btn-default"><span class="glyphicon glyphicon-log-out"></span> Logout</button>
+      </div>
     </div>
     <br>
   </div>
 </template>
 
 <script>
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth'
+import { collection, onSnapshot, getFirestore } from 'firebase/firestore'
+
 export default {
   name: 'CityList',
-  data () {
+  data() {
     return {
       msg: 'EGCI427 Practice',
       city: null,
       cities: {},
-      editName: []
+      cityIds: {},
+      editName: [],
+      auth: getAuth(),
+      isLoggedIn: false,
     }
   },
-  created () {
+  created() {
     console.log('City List')
+
+    onAuthStateChanged(this.auth, (user) => {
+      if(user) {
+        this.isLoggedIn = true
+      } else {
+        this.isLoggedIn = false
+      }
+    })
+
+    const db = getFirestore()
+    const docRef = collection(db, "cities")
+    onSnapshot(docRef, (snapshot) => {
+      this.cities = snapshot.docs.map(doc => doc.data())
+      this.cityIds = snapshot.docs.map(doc => doc.id)
+    })
   },
   methods: {
-    logout () {
+    logout() {
       console.log('logout')
     }
   }
@@ -60,21 +81,26 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   /* display: inline-block; */
   margin: 0 10px;
 }
+
 a {
   color: #ffffff;
 }
-p.citydetail{
+
+p.citydetail {
   text-align: justify;
 }
 </style>
